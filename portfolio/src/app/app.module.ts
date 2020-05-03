7cd21f4fb4ecbcbefe6660aa7b7cd3bff02bc974
Srikar Phani Kumar Marti <psrikarm@gmail.com>
2020-05-03 02:45:15 -0400

    Background Images setup

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { LibraryModule } from './lib/library-module';
 import { HomeComponent } from './ui/home/home.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
